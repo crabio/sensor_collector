8e308184b834aa6b31ad1c7c9796de677f447a54
Ivan Shchukin <shchukin.ivan.v@gmail.com>
2024-07-22 14:29:55 +0200

    Create log init function

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_foreground_task/ui/with_foreground_task.dart';
 import 'package:logging/logging.dart';
 import 'package:sensor_collector/bloc/mobile/bloc.dart';
 import 'package:sensor_collector/bloc/wearable/bloc.dart';
+import 'package:sensor_collector/repositories/sensor_collector_foreground_service.dart';
+import 'package:sensor_collector/utils/log.dart';
 import 'package:sensor_collector/utils/platform.dart';
 import 'package:sensor_collector/view/page/mobile_page.dart';
 import 'package:sensor_collector/view/page/wearable_page.dart';
 
 Future<void> main() async {
-  Logger.root.level = Level.FINE;
-  Logger.root.onRecord.listen((record) {
-    print('${record.level.name}: ${record.time}: ${record.message}');
-  });
+  initLogger(level: Level.FINE);
 
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Request required permissions
+  await SensorCollectorForegroundService.requestPermission();
+
   runApp(SensorCollectorApp(await isWearable()));
 }
 
 class SensorCollectorApp extends StatelessWidget {
-  final Logger _log = Logger('SensorCollectorApp');
   final bool isWearable;
 
-  SensorCollectorApp(this.isWearable, {super.key});
+  const SensorCollectorApp(this.isWearable, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -36,6 +39,7 @@ class SensorCollectorApp extends StatelessWidget {
 }
 
 class SensorCollectorBlocProvider extends StatelessWidget {
+  final Logger _log = Logger('SensorCollectorBlocProvider');
   final bool isWearable;
 
   SensorCollectorBlocProvider(this.isWearable, {super.key});
