b847bc881e9eb1cba0b079618caf540809e631bc
Ivan Shchukin <shchukin.ivan.v@gmail.com>
2024-09-11 21:46:27 +0200

    feat(wear): :bug: Add missed watch shape for wear app

--- lib/view/page/wearable_page.dart
+++ lib/view/page/wearable_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sensor_collector/bloc/wearable/bloc.dart';
 import 'package:sensor_collector/view/widget/elapsed_timer.dart';
 import 'package:sensor_collector/view/widget/start_collect_btn.dart';
+import 'package:wear_plus/wear_plus.dart';
 
 class WearablePage extends StatefulWidget {
   const WearablePage({super.key});
@@ -16,18 +17,28 @@ class _WearablePageState extends State<WearablePage> {
   Widget build(BuildContext context) {
     return BlocBuilder<WearableBloc, WearableState>(
       builder: (context, state) {
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElapsedTimerWidget(state.elapsed),
-              const SizedBox(height: 20),
-              StartCollectButton(
-                () => context.read<WearableBloc>().add(PressCollectingButton()),
-                state.isCollectingData,
-              ),
-            ],
-          ),
+        return WatchShape(
+          builder: (BuildContext context, WearShape shape, Widget? child) {
+            return AmbientMode(
+              builder: (context, mode, child) {
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      ElapsedTimerWidget(state.elapsed),
+                      const SizedBox(height: 20),
+                      StartCollectButton(
+                        () => context
+                            .read<WearableBloc>()
+                            .add(PressCollectingButton()),
+                        state.isCollectingData,
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
         );
       },
     );
